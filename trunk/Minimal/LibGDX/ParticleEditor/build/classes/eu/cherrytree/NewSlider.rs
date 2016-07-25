28f00de7e43e541ed212388f54c01f735c3adb05
yezu <yezu@737b3d22-c0a7-4646-af83-2f67aba5ccec>
2016-07-25 21:12:13 +0000

    Removed not necessary files.

--- trunk/Minimal/LibGDX/ParticleEditor/build/classes/eu/cherrytree/NewSlider.rs
+++
@@ -1,4 +0,0 @@
-com.badlogic.gdx.tools.particleeditor.NewSlider$3
-com.badlogic.gdx.tools.particleeditor.NewSlider$2
-com.badlogic.gdx.tools.particleeditor.NewSlider$1
-com.badlogic.gdx.tools.particleeditor.NewSlider
