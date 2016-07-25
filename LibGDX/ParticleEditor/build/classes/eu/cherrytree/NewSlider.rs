c6089ae697f900fa8a259918ce5c7898f7cdd6f2
yezu <Leshy-CherryTree@users.noreply.github.com>
2016-07-25 21:09:38 +0000

    Added libGDX based particle 2d particle system and editor.

---
+++ LibGDX/ParticleEditor/build/classes/eu/cherrytree/NewSlider.rs
@@ -0,0 +1,4 @@
+com.badlogic.gdx.tools.particleeditor.NewSlider$3
+com.badlogic.gdx.tools.particleeditor.NewSlider$2
+com.badlogic.gdx.tools.particleeditor.NewSlider$1
+com.badlogic.gdx.tools.particleeditor.NewSlider
